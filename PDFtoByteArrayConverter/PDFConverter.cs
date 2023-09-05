1dc01c619105ebb2abaaf64748a7deabe66b1ab4
Shubh More <shubham.more@klingelnberg.com>
2023-09-05 12:12:39 +0530

    Convert PDF to Byte Array

---
+++ PDFtoByteArrayConverter/PDFConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PDFConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter File Path: ");
+            byte[] PDFByteArray=PDFToByteArray(Console.ReadLine());
+
+            foreach (byte b in PDFByteArray)
+            {
+                Console.WriteLine(b);
+            }
+        }
+
+        static byte[]? PDFToByteArray(string pdfFilePath)
+        {
+            try
+            {
+                byte[] bytes = System.IO.File.ReadAllBytes(pdfFilePath);
+                return bytes;
+            }
+            catch (IOException exception)
+            {
+                Console.WriteLine(exception.Message + "\nPlease input valid file path.");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            return null;
+        }
+    }
+}
